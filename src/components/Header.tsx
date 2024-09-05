3204ec80eedca6844682d1342c86c7b653e0359e
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-09-04 23:28:58 -0300

    create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+  return (
+    <header className="header-container">
+      <img
+        src="src/assets/bg-cafe.jpg"
+        alt="coffe cover"
+        className="header-image"
+      />
+    </header>
+  );
+};
