8031101eba8cb1f699a3fc8424a0474efe2b9558
Adrián Fonseca Vega <afonsecav88@gmail.com>
2024-09-06 22:48:30 -0300

    fixed position vector imagen at container header component

--- src/components/ContainerContainerHeader.tsx
+++ src/components/ContainerContainerHeader.tsx
@@ -1,12 +1,13 @@
 export const ContainerContainerHeader = () => {
   return (
-    <>
+    <section className="container-coffee-header">
+      <img src="src/assets/vector.svg" alt="vector header" />
       <h1>Our Collection</h1>
       <p>
         Introducing our Coffee Collection, a selection of unique coffees from
         different roast types and origins, expertly roasted in small batches and
         shipped fresh weekly.
       </p>
-    </>
+    </section>
   );
 };
